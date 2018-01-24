3042d3e0477bc651b3ae361075a2ce29341d17a2
Peter Street <PeterBStreet@gmail.com>
2018-01-24 08:41:53 -0700

    completed function

--- snap-brig.php
+++ snap-brig.php
@@ -6,14 +6,16 @@
  * Time: 8:10 AM
  */
 
-   $crime = array("larsony"=>"Larsony", "embezzlement"=>"embezzlement", "praisingComcast"=>"praisingComcast", "tamperingWithReplicators"=>"tamperingWithReplicators", "vehicularManslaughter"=>"vehicularManslaughter");
 
-   $prisoners = array("brent"=>"brent", "miguel"=>"miguel", "nat"=>"nat");
+$crime = array("larsony"=>"Larsony", "embezzlement"=>"embezzlement", "praisingComcast"=>"praisingComcast", "tamperingWithReplicators"=>"tamperingWithReplicators", "vehicularManslaughter"=>"vehicularManslaughter");
+
+$prisoners = array("brent"=>"brent", "miguel"=>"miguel", "nat"=>"nat");
 
 echo $crime[array_rand($crime)];
-
 echo $prisoners[array_rand($prisoners)];
 
+
+/*
 $input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
 $rand_keys = array_rand($input, 2);
 echo $input[$rand_keys[0]] . "\n";
