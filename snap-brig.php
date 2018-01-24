df0c2dfa57e9bd5a47d2a5e23a0c9b81c04c3fbe
Peter Street <PeterBStreet@gmail.com>
2018-01-24 08:24:33 -0700

    Initial function

--- snap-brig.php
+++ snap-brig.php
@@ -5,3 +5,17 @@
  * Date: 1/24/18
  * Time: 8:10 AM
  */
+
+   $crime = array("larsony"=>"Larsony", "embezzlement"=>"embezzlement", "praisingComcast"=>"praisingComcast", "tamperingWithReplicators"=>"tamperingWithReplicators", "vehicularManslaughter"=>"vehicularManslaughter");
+
+   $prisoners = array("brent"=>"brent", "miguel"=>"miguel", "nat"=>"nat");
+
+echo $crime[array_rand($crime)];
+
+echo $prisoners[array_rand($prisoners)];
+
+$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+$rand_keys = array_rand($input, 2);
+echo $input[$rand_keys[0]] . "\n";
+echo $input[$rand_keys[1]] . "\n";
+?>
